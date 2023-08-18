f5ecbdb0bde7c8839bc7581dae91fb93bd79f6df
satishVenka <satishvenket26@gmail.com>
2023-08-18 12:20:15 +0530

    message

---
+++ src/test/java/com/Maven/Testuing.java
@@ -0,0 +1,13 @@
+package com.Maven;
+
+import org.testng.annotations.Test;
+
+public class Testuing {
+
+	@Test
+	public  void Test() {
+		// TODO Auto-generated constructor stub
+		System.out.println("hai");
+	}
+
+}

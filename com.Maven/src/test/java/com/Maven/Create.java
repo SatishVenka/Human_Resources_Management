6889450024bd013d1f4fcc927572a6f687c0aa3f
satish <126506259+SatishVenka@users.noreply.github.com>
2023-08-18 09:13:05 +0530

    Update Create.java

--- com.Maven/src/test/java/com/Maven/Create.java
+++ com.Maven/src/test/java/com/Maven/Create.java
@@ -6,7 +6,7 @@ public class Create {
 @Test(groups="Regression")
 public void create()
 {
-	System.out.println("---created new repo--");
+	System.out.println("---created new");
 }
 
 @Test(groups="Smoke")
